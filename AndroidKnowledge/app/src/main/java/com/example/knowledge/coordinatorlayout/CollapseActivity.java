a6825a9ad34739d5a8790c6c9b00e140233dfae2
GaoLei <gaolei20@guazi.com>
2019-09-09 07:43:03 +0800

    增加CollapseViewPagerActivity

--- AndroidKnowledge/app/src/main/java/com/example/knowledge/coordinatorlayout/CollapseActivity.java
+++ AndroidKnowledge/app/src/main/java/com/example/knowledge/coordinatorlayout/CollapseActivity.java
@@ -1,4 +1,4 @@
-package com.example.knowledge.newui;
+package com.example.knowledge.coordinatorlayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
---
+++ AndroidKnowledge/app/src/main/java/com/example/knowledge/coordinatorlayout/ToobarLisenner.java
@@ -0,0 +1,12 @@
+package com.example.knowledge.coordinatorlayout;
+
+/**
+ * Created by 路很长~ on 2018/4/10.
+ */
+
+public interface ToobarLisenner {
+    void hide();
+
+    void visible();
+
+}
