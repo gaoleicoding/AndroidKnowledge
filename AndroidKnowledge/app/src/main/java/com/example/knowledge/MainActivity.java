178869948ae0073379173bbed335b2a78afe973f
Lei Gao <gaolei@elex-tech.com>
2020-01-07 19:20:41 +0800

    [add]1、增加DecryptActivity

--- AndroidKnowledge/app/src/main/java/com/example/knowledge/MainActivity.java
+++ AndroidKnowledge/app/src/main/java/com/example/knowledge/MainActivity.java
@@ -5,21 +5,47 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.knowledge.adapter.ItemAdapter;
+import com.example.knowledge.decrypt.DecryptActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    List<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.title_text).setOnClickListener(new View.OnClickListener() {
+        list.add("EncryptActivity（加解密）");
+
+        RecyclerView recyclerview = findViewById(R.id.recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerview.setLayoutManager(layoutManager);
+        ItemAdapter itemAdapter = new ItemAdapter(this, list);
+        recyclerview.setAdapter(itemAdapter);
+        recyclerview.addItemDecoration(
+                new DividerItemDecoration(MainActivity.this,
+                        DividerItemDecoration.VERTICAL
+                )
+        );
+
+        itemAdapter.setOnItemClickLitener(new ItemAdapter.OnItemClickLitener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent("com.example.second");
-                startActivity(intent);
+            public void onItemClick(View v) {
+                int position = recyclerview.getChildAdapterPosition(v);
+                if(position==0){
+                    startActivity(new Intent(MainActivity.this, DecryptActivity.class));
+                }
             }
         });
-
     }
 
 }
---
+++ AndroidKnowledge/app/src/main/java/com/example/knowledge/decrypt/DecryptActivity.java
@@ -0,0 +1,18 @@
+package com.example.knowledge.decrypt;
+
+import android.os.Bundle;
+
+import com.example.knowledge.BaseActivity;
+import com.example.knowledge.R;
+
+public class DecryptActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collapse);
+
+    }
+
+}
