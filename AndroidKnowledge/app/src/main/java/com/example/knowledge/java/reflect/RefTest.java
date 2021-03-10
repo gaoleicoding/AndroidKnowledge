028c4b2e0dce62f41f9bc3c169d6738594687168
GaoLei <gaolei20@guazi.com>
2021-03-10 22:58:04 +0800

    1、增加反射例子

---
+++ AndroidKnowledge/app/src/main/java/com/example/knowledge/java/reflect/RefTest.java
@@ -0,0 +1,19 @@
+package com.example.knowledge.java.reflect;
+
+import java.lang.reflect.Method;
+
+public class RefTest {
+    public static void main(String[] args) {
+        try {
+            Class clazz = Class.forName("com.zy.java.RefTest");
+            Object refTest = clazz.newInstance();
+            Method method = clazz.getDeclaredMethod("refMethod");
+            method.invoke(refTest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void refMethod() {
+    }
+}
