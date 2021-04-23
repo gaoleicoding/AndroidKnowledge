10c6e9da4d8f8e800beb0faafb8b44aad8d95fd8
GaoLei <gaolei20@guazi.com>
2021-04-23 09:02:01 +0800

    1、完善反射的使用

--- AndroidKnowledge/app/src/main/java/com/example/knowledge/Demo2Activity.java
+++ AndroidKnowledge/app/src/main/java/com/example/knowledge/Demo2Activity.java
@@ -1,9 +1,12 @@
 package com.example.knowledge;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.IBinder;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Set;
 
 public class Demo2Activity extends AppCompatActivity {
 
@@ -11,5 +14,9 @@ public class Demo2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Set<String> routerMap = null;
+
+        getSharedPreferences("AROUTER_SP_CACHE_KEY", Context.MODE_PRIVATE).edit().putStringSet("AROUTER_SP_KEY_MAP", routerMap).apply();
+
     }
 }
--- AndroidKnowledge/java/src/main/java/com/gl/java/reflect/User.java
+++ AndroidKnowledge/java/src/main/java/com/gl/java/reflect/User.java
@@ -1,13 +1,36 @@
 package com.gl.java.reflect;
 
-import java.util.Arrays;
-
 class User {
-    public static void method(String... strings) {
-        System.out.println(Arrays.toString(strings));
+
+    private String name;
+    private int age;
+
+    public User() {
+
     }
 
-    private void method(int... ints) {
-        System.out.println(Arrays.toString(ints));
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    private User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
