e18edfbf88c0ccd486e5b4e0aa1db558ffee2fe8
leigao9 <leigao9@iflytek.com>
2022-04-26 11:20:31 +0800

    feat(Process):增加位前后台切换判断

--- AndroidKnowledge/app/src/main/java/com/example/knowledge/process/ProcessAliveActivity.java
+++ AndroidKnowledge/app/src/main/java/com/example/knowledge/process/ProcessAliveActivity.java
@@ -1,8 +1,13 @@
 package com.example.knowledge.process;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
+import androidx.lifecycle.ProcessLifecycleOwner;
 
 import com.example.knowledge.R;
 
@@ -12,6 +17,20 @@ public class ProcessAliveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_process_alive);
-    }
+        ProcessLifecycleOwner.get().getLifecycle().addObserver(new MyLifecycleObserver());
 
+    }
+    private class MyLifecycleObserver implements LifecycleObserver {
+
+        // 方法名随便取，注解才是重点
+        @OnLifecycleEvent(Lifecycle.Event.ON_START)
+        void onForeground() {
+            Log.i("LifecycleObserver", "应用回到前台");
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+        void onBackground() {
+            Log.i("LifecycleObserver", "应用退到后台");
+        }
+    }
 }
